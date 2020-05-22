7bc67250141ba62673a61e259c6f36a95880ac72
alexb <alex.baranchuk121@gmail.com>
2020-05-22 21:37:03 +0300

    Naming - Change message to game_result

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def main():
     args = parser.parse_args()
     move_iterator = create_move_iterator_from_move_file(args.path)
     game = CheckersGame(CheckerBoardFactory.build_board_from_preset(CheckerBoardPresets.standard_8_by_8))
-    message = game.run_game(move_iterator)
-    print(f'{os.path.basename(args.path)} - {message}')
+    game_result = game.run_game(move_iterator)
+    print(f'{os.path.basename(args.path)} - {game_result}')
 
 
 if __name__ == '__main__':
